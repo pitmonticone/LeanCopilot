288aa1db8623fbea168f59c1fe57c116f998a837
Kaiyu Yang <kaiyuy@cs.princeton.edu>
2023-10-16 22:07:57 -0500

    simplify

--- lakefile.lean
+++ lakefile.lean
@@ -1,30 +1,16 @@
 import Lake
-open Lake DSL System Lean Elab
+open Lake DSL Lean
 
 
-inductive SupportedArch where
+inductive SupportedArch
   | x86_64
   | arm64
 
 
 def getArch? : BaseIO (Option SupportedArch) := do
   let proc := IO.Process.output {cmd := "uname", args := #["-m"], stdin := .null}
-  let .ok output ← proc.toBaseIO
-    | return none
-  let arch := output.stdout.trim
-  if arch ∈ ["arm64", "aarch64"] then
-    return some .arm64
-  else if arch == "x86_64" then
-    return some .x86_64
-  else
-    return none
-
-
-def getArch : IO SupportedArch := do
-  if let some arch ← getArch? then 
-    return arch 
-  else
-    error "Unknown architecture"
+  let _ ← proc.toBaseIO
+  return some .x86_64
 
 
 elab "is_arm?" : term => do
